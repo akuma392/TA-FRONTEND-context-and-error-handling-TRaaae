b655053895b0b84854e18a2d32d5add245ce2ea8
Abhishek <abhivision3@gmail.com>
2021-05-24 20:50:29 +0530

    Add files via upload

--- block-BRaadi/code/src/components/App.js
+++ block-BRaadi/code/src/components/App.js
@@ -1,15 +1,30 @@
-import React from "react";
-import Header from "./Header";
-import Main from "./Main";
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import { ErrorBoundary } from 'react-error-boundary';
+
+function ErrorFallBack({ error }) {
+  console.log(error, 'Error fall back');
+  return (
+    <div>
+      <p>Something went wrong:{error}</p>
+    </div>
+  );
+}
 
 class App extends React.Component {
+  // state = {
+  //   isDarkMode: false,
+  // };
   render() {
     let { isDarkMode } = this.state;
     return (
-      <>
-        <Header isDarkMode={isDarkMode} />
-        <Main isDarkMode={isDarkMode} />
-      </>
+      <div>
+        <ErrorBoundary FallbackComponent={ErrorFallBack}>
+          <Header isDarkMode={isDarkMode} />
+          <Main isDarkMode={isDarkMode} />
+        </ErrorBoundary>
+      </div>
     );
   }
 }
